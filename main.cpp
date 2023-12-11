bf88304c0b3c63c09def995034a655b45e4e3853
mbrizic <brizic.mario@gmail.com>
2023-12-11 22:47:53 +0100

    Show active windows in real time

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <windows.h>
+#include <set>
 #include <iostream>
 using namespace std;
 
-// Global hook handle
-HHOOK g_hHook;
+HHOOK g_keyboardHook;
 const DWORD ralt = 165;
 const DWORD rshift = 161;
 
@@ -27,22 +27,72 @@ bool isRaltPressed = false;
 bool isRshiftPressed = false;
 bool debug = true;
 
-// Keyboard hook procedure
-LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+struct WindowInfo
+{
+    HWND hwnd;
+    std::string name;
+    LONG left;
+    LONG top;
+    LONG right;
+    LONG bottom;
+
+    bool isHidden() {
+        return left < 0 && right < 0 && top < 0 && bottom < 0;
+    }
+};
+
+// We need to override this for set ordering and uniqueness
+bool operator<(const WindowInfo& lhs, const WindowInfo& rhs)
+{
+    return lhs.hwnd < rhs.hwnd;
+}
+
+std::set<WindowInfo> activeWindows;
+
+std::string getWindowName(HWND hwnd) {
+    char nameBuffer[256];
+
+    GetWindowTextA(hwnd, nameBuffer, sizeof(nameBuffer));
+
+    return std::string(nameBuffer);
+}
+
+void registerNewWindow(HWND hwnd, RECT wSize) {
+    std::string name = getWindowName(hwnd);
+
+    if (IsWindowVisible(hwnd) && !IsIconic(hwnd))
+    {
+        WindowInfo windowInfo = {
+            hwnd,
+            name,
+            wSize.left,
+            wSize.top,
+            wSize.right,
+            wSize.bottom
+        };
+
+        if (!windowInfo.isHidden()) {
+            activeWindows.insert(windowInfo);
+        }
+    }
+}
+
+LRESULT CALLBACK KeyboardHookProc(int nCode, WPARAM wParam, LPARAM lParam) {
     if (nCode >= 0) {
         if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) {
             KBDLLHOOKSTRUCT* pKeyStruct = (KBDLLHOOKSTRUCT*)lParam;
             DWORD keyCode = pKeyStruct->vkCode;
 
             char keyChar = MapVirtualKey(keyCode, 2);
-            
+
             if (isprint(keyChar) && isRaltPressed && isRshiftPressed) {
                 printf("Alt + Shift + %c\n", keyChar);
-            }else 
-                
-            if (isprint(keyChar) && isRaltPressed) {
-                printf("Alt + %c\n", keyChar);
             }
+            else
+
+                if (isprint(keyChar) && isRaltPressed) {
+                    printf("Alt + %c\n", keyChar);
+                }
 
             if (debug) {
                 printf("PRESSED: %d\n", keyCode);
@@ -81,13 +131,17 @@ LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
     }
 
     // Call the next hook in the hook chain
-    return CallNextHookEx(g_hHook, nCode, wParam, lParam);
+    return CallNextHookEx(g_keyboardHook, nCode, wParam, lParam);
+}
+
+bool IsMainWindow(HWND hwnd) {
+    LONG style = GetWindowLong(hwnd, GWL_STYLE);
+    return ((style & WS_CHILD) == 0) && (GetWindow(hwnd, GW_OWNER) == nullptr) && IsWindowVisible(hwnd);
 }
 
 BOOL CALLBACK handleWindow(HWND hwnd, LPARAM lParam) {
-
-    // Exit early if window is not visible
-    if (!IsWindowVisible(hwnd)) {
+    // Exit early if window is popup or something not a real window
+    if (!IsMainWindow(hwnd)) {
         return TRUE;
     }
 
@@ -107,42 +161,70 @@ BOOL CALLBACK handleWindow(HWND hwnd, LPARAM lParam) {
 
     GetWindowText(hwnd, buffer, length + 1);
 
+    delete[] buffer;
+
     RECT wSize = info.rcWindow;
 
-    char nameBuffer[256];
-    if (GetWindowTextA(hwnd, nameBuffer, sizeof(nameBuffer)) > 0) {
-        std::cout << "Window Title: " << nameBuffer << std::endl;
-    }
-
-    if (wSize.left > 0 || wSize.top > 0 || wSize.right > 0 || wSize.bottom > 0) {
-        printf("  LEFT %d, TOP %D, RIGHT %d, BOTTOM %d\n", wSize.left, wSize.top, wSize.right, wSize.bottom);
-    }
-
-    delete[] buffer;
-    //printf("%d\n", buffer);
+    registerNewWindow(hwnd, wSize);
 
     return TRUE;
 }
 
-int main() {
-    // Set the keyboard hook
-    g_hHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, GetModuleHandle(NULL), 0);
+// Listens for any window changes (create/destroy/update), updates the `activeWindows` list
+// and prints out the latest state.
+void CALLBACK WindowWatcherHookProc(
+    HWINEVENTHOOK hWinEventHook,
+    DWORD         event,
+    HWND          hwnd,
+    LONG          idObject,
+    LONG          idChild,
+    DWORD         dwEventThread,
+    DWORD         dwmsEventTime
+)
+{
+    // Exit early if window is popup or something not a real window
+    if (!IsMainWindow(hwnd)) {
+        return;
+    }
 
-    if (g_hHook == NULL) {
-        // Hook installation failed
-        // Handle the error here
+    activeWindows.clear();
+    EnumWindows(handleWindow, NULL);
+
+    std::system("cls");
+    std::cout << "Currently active windows: " << activeWindows.size() << std::endl;
+    for (const WindowInfo& window : activeWindows)
+    {
+        std::cout << "  " << window.hwnd << " - " << window.name << std::endl;
+        printf("    %d %d %d %d\n", window.left, window.top, window.right, window.bottom);
+    }
+}
+
+int main() {
+    g_keyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardHookProc, GetModuleHandle(NULL), 0);
+
+    if (g_keyboardHook == NULL) {
+        std::cerr << "Error setting up low level keyboard hook" << std::endl;
         return 1;
     }
 
-    EnumWindows(handleWindow, NULL);
+    HWINEVENTHOOK hWindowWatcherHook = SetWinEventHook(EVENT_OBJECT_CREATE, EVENT_OBJECT_REORDER, nullptr, WindowWatcherHookProc, 0, 0, WINEVENT_OUTOFCONTEXT);
+
+    if (hWindowWatcherHook == nullptr)
+    {
+        std::cerr << "Error setting up WinEventHook" << std::endl;
+        return 1;
+    }
+
     // Message loop or other application logic goes here
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-    // Unhook the keyboard hook when you're done
-    UnhookWindowsHookEx(g_hHook);
+
+    // Remove hooks after done
+    UnhookWindowsHookEx(g_keyboardHook);
+    UnhookWinEvent(hWindowWatcherHook);
 
     return 0;
 }
